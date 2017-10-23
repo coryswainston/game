339f6ffb0d58f4c40c451d29c9184dab3034e5a1
Cory Swainston <swainston.cory89@gmail.com>
2017-10-23 17:39:13 -0600

    Add sheepPile

--- Game/app/src/main/java/com/coryswainston/game/objects/Llama.java
+++ Game/app/src/main/java/com/coryswainston/game/objects/Llama.java
@@ -3,8 +3,12 @@ package com.coryswainston.game.objects;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
 import com.coryswainston.game.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Cory Swainston
@@ -16,6 +20,7 @@ public class Llama extends Sprite {
     private int yCeiling;
     private boolean ducking;
     private int duckTimer;
+    private List<Sheep> sheepPile = new ArrayList<>();
 
     private final int GRAVITY = 5;
 
@@ -32,8 +37,8 @@ public class Llama extends Sprite {
         duckTimer = 0;
     }
 
+    @Override
     public void update(){
-
         x += dx;
         y += dy;
         if (y < yFloor){
@@ -46,6 +51,20 @@ public class Llama extends Sprite {
             y = yFloor;
             dy = 0;
         }
+        for (Sheep sheep : sheepPile) {
+            sheep.setDx(0);
+            sheep.setDy(0);
+            if (sheep.isSeated) {
+                sheep.setX(x);
+            } else {
+                sheep.setX(sheep.getX() + ((sheep.getX() - x) > 0 ? 1 : -1));
+                sheep.setY(new Float(sheep.getA() * Math.pow(x - sheep.getX(), 2) - yFloor + y));
+                Log.d("Here's some info", "A: " + sheep.getA() + ", " + sheep.getX() + ", " + sheep.getY());
+                if (sheep.getY() == y) {
+                    sheep.isSeated = true;
+                }
+            }
+        }
     }
 
     public void jump() {
@@ -53,11 +72,18 @@ public class Llama extends Sprite {
     }
 
     public void duck() {
-
+        ducking = true;
     }
 
-    public void release(){
+    public List<Sheep> getSheepPile() {
+        return sheepPile;
+    }
 
+    public void addToPile(Sheep sheep) {
+        sheepPile.add(sheep);
+        float xDelta = sheep.getX() - x;
+        double a = y / Math.pow(xDelta, 2);
+        sheep.setA(a);
     }
 
     public void setFloor(int yFloor){ this.yFloor = yFloor; }
