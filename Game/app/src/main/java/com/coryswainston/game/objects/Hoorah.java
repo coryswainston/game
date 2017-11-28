4b24bc0e71c73ac95201862f9dc1a4ffee1a2015
Cory Swainston <swainston.cory89@gmail.com>
2017-11-27 20:34:05 -0700

    Concept of a Hoorah

---
+++ Game/app/src/main/java/com/coryswainston/game/objects/Hoorah.java
@@ -0,0 +1,23 @@
+package com.coryswainston.game.objects;
+
+import android.graphics.Bitmap;
+
+/**
+ * A class for objects to be temporarily drawn
+ *
+ * @author coryswainston
+ */
+
+public class Hoorah {
+    public static final int TIME_LONG = 50;
+    public static final int TIME_MED = 25;
+    public static final int TIME_SHORT = 10;
+
+    private int counter;
+
+    // TODO implement to take drawText() params and an optional background
+    // TODO contain a draw() function that uses DrawingHelper.drawText()
+    // TODO implement container class that draws Hoorahs until they're timed out
+    // TODO create a Hoorah when picking up a sheep (+100), new high score, etc.
+    // TODO potentially attach these to sound effects
+}
